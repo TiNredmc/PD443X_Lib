c0c341268f825025c8dddd3f1b5050131a6b2aae
TiN_redmc <thipok17@hotmail.com>
2018-03-01 08:09:36 +0700

    Added SetBrigthness
    Added SetBrigthness and added a description individually on all statements.

--- PD443X.h
+++ PD443X.h
@@ -6,13 +6,13 @@
 class PD443X
 {
 public:
-     PD443X(int Adr0,int Adr1,int Adr2,int CE,int RST,int               WR,int latch,int clock,int data);
-     void SendByte(char a,byte column); 
-     void ClrDisp();
-     void SendStringScroll(String text,int Speed);
-     void SetBrigthness(int B);
-     void CtrlWord(char X);
-     void LampTest(int tog);
+     PD443X(int Adr0,int Adr1,int Adr2,int CE,int RST,int WR,int latch,int clock,int data);//set pinout
+     void SendByte(char a,byte column); //send individually ascii to individually digit on display
+     void ClrDisp();//clear display by using controlWord
+     void SendStringScroll(String text,int Speed);//send text string and set scrolling speed
+     void SetBrigthness(int B);/set brigthness 0 1 2 3 (0,25,50,100)
+     void CtrlWord(char X);//special controlWord (See in datasheet)
+     void LampTest(int tog);// 1 to run lamptest 0 to exit lamp test
 private:
   int _Adr0;
   int _Adr1;
